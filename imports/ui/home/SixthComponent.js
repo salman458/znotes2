8a873829540d458f6bf5486b83b85bd99832b67b
armanmac <arman.piloyan@gmail.com>
2019-09-29 15:20:44 +0400

    sixth component complete

--- imports/ui/home/SixthComponent.js
+++ imports/ui/home/SixthComponent.js
@@ -3,9 +3,24 @@ import React from "react";
 
 export default () => {
     return (
-        <div>
-            <h2>First Component</h2>
-            <div>
+        <div className="six">
+
+            <div className="sixth-component">
+                <h1 style={{color:"white"}}><b>Contact</b></h1>
+
+                <div className="mapouter">
+                    <div className="gmap_canvas">
+                        <h2><b>Location</b></h2>
+                        <br/>
+                        <br/>
+                        <iframe width="600" height="500" id="gmap_canvas"
+                                src="https://maps.google.com/maps?q=university%20of%20san%20francisco&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                                frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
+                        <a href="https://www.embedgooglemap.net/blog/nordvpn-coupon-code/">norevpn</a></div>
+                </div>
+            </div>
+
+            <div className="socialCont">
                 <ul className="social">
                     <li className="team"><p>Made with love by ZNotes team</p></li>
                     <li>
