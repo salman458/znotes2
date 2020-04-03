b34f24db53ea5b30676c1b7c7386fd62fea400d5
abbas ashraf <amughal@digital-dividend.com>
2020-04-03 16:30:19 +0500

    buttons on team role

--- client/components/organisms/Sidebar/SidebarContent/index.js
+++ client/components/organisms/Sidebar/SidebarContent/index.js
@@ -220,7 +220,7 @@ const getAllCardsByModuleSlugName=async()=>{
         {allChapters.map((chapter, i) => (
           <div key={i}>
             <ListItem key={chapter._id} {...chapter} onCardClick={(cardData) => onCardClick(cardData, chapter)} />
-
+            {isTeamRole && (
 <div>
 <button
               className="MuiButtonBase-root MuiButton-root MuiButton-contained makeStyles-root-98 MuiButton-containedPrimary"
@@ -236,7 +236,7 @@ const getAllCardsByModuleSlugName=async()=>{
             >
               Delete
             </button>
-</div>
+</div>)}
             {isTeamRole && (
             <button
               className="MuiButtonBase-root MuiButton-root MuiButton-contained makeStyles-root-98 MuiButton-containedPrimary"
