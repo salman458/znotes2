05ef92c7d9a66b008cadb0fd31edb75a055bf13a
abbas ashraf <amughal@digital-dividend.com>
2020-04-13 18:25:12 +0500

    f

--- client/components/pages/Register/index.js
+++ client/components/pages/Register/index.js
@@ -146,6 +146,7 @@ const Register = () => {
             body: {
               userId,
               fields: {
+                firstName,
                 lastName,
                 gender,
                 livingPlace,
