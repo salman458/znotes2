bc0443227ae760197e979c670da94938271e104e
Firdous Ali <fali@digital-dividend.com>
2020-04-14 15:02:50 +0500

    lint + ref

--- client/components/organisms/Sidebar/SidebarContent/index.js
+++ client/components/organisms/Sidebar/SidebarContent/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { lighten } from '@material-ui/core/styles';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { lighten } from "@material-ui/core/styles";
 import {
   Text,
   Title,
@@ -10,15 +10,15 @@ import {
   ProgressBar,
   Button,
   TextField,
-  Link
-} from '/client/components/atoms';
-import { Request, USER_PERMISSIONS } from '/client/utils';
-import { ChevronLeft, Chapters } from '/client/components/icons';
-import Subjects from '/client/components/molecules/SubjectCard/subjectData';
-import ListItem from '../ListItem';
-import ClosePopup from '/client/components/molecules/ClosePopup';
-import  ConfirmationDialog from "/client/components/molecules/ConfirmationDialog";
-import { SanitizeName } from '/client/utils';
+  Link,
+} from "/client/components/atoms";
+import { Request, USER_PERMISSIONS } from "/client/utils";
+import { ChevronLeft, Chapters } from "/client/components/icons";
+import Subjects from "/client/components/molecules/SubjectCard/subjectData";
+import ListItem from "../ListItem";
+import ClosePopup from "/client/components/molecules/ClosePopup";
+import ConfirmationDialog from "/client/components/molecules/ConfirmationDialog";
+import { SanitizeName } from "/client/utils";
 import { useGlobal, setGlobal } from "reactn";
 
 const SidebarContent = ({
@@ -37,23 +37,48 @@ const SidebarContent = ({
   const isTeamRole = role === USER_PERMISSIONS.editor;
   const sanitizedSubjectName = SanitizeName(subject);
   const { color } = Subjects[sanitizedSubjectName] || {};
-  const primaryColor = color || '#D82057';
+  const primaryColor = color || "#D82057";
   const secondaryColor = lighten(primaryColor, 0.5);
   const [open, setOpen] = useState(false);
   const [editPopupOpen, setEditPopupOpen] = useState(false);
   const [allChapters, setChapters] = useState(chapters);
   const [selectedChapter, setSelectedChapter] = useState("");
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [cards] = useGlobal("cardsData");
+
+  const saveLastPosition = async () => {
+    const progressValue = getProgressValue();
+    const urlToSave = `/${boardSlugName}/${levelSlugName}/${subjectSlugName}/${moduleSlugName}?chapterId=${chapterId}&cardId=${cardId}`;
+    const subject = await Request({
+      action: "getSubjectBySlug",
+      body: subjectSlugName,
+    });
+
+    const addLastPosition = await Request({
+      action: "addLastPosition",
+      body: {
+        userId: Meteor.userId(),
+        subject: {
+          id: Meteor.userId(),
+          position: urlToSave,
+          progress: progressValue,
+          moduleName: subject.moduleName,
+          boardName: subject.boardName,
+          levelName: subject.levelName,
+          subjectName: subject.name,
+        },
+      },
+    });
+  };
 
   useEffect(() => {
     setChapters(chapters);
   }, [chapters]);
 
-  useEffect(()=>{
-    saveLastPosition()
-  },[cardId])
+  useEffect(() => {
+    saveLastPosition();
+  }, [cardId]);
 
   const addCard = async (chapId) => {
     const url = `/editor/${boardSlugName}/${levelSlugName}/${subjectSlugName}/${moduleSlugName}?chapterId=${chapId}&cardId=${1}`;
@@ -69,9 +94,10 @@ const SidebarContent = ({
       }
       const value = ((cardIndex + 1) / totalCards) * 100;
       return value.toFixed(0);
-    } return 0;
+    }
+    return 0;
   };
-  
+
   const openPopup = () => {
     setOpen(true);
   };
@@ -79,10 +105,10 @@ const SidebarContent = ({
     setOpen(false);
   };
 
-  const openEditPopup =(selectedChapter)=>{
+  const openEditPopup = (selectedChapter) => {
     setEditPopupOpen(true);
     setSelectedChapter(selectedChapter);
-  }
+  };
   const onCloseEditPopup = () => {
     setEditPopupOpen(false);
   };
@@ -92,7 +118,7 @@ const SidebarContent = ({
   const onChapterChange = (e) => {
     setSelectedChapter({
       ...selectedChapter,
-      name:e.target.value
+      name: e.target.value,
     });
   };
   const onSubmit = async () => {
@@ -102,7 +128,7 @@ const SidebarContent = ({
       cards: [],
     };
     const itemId = await Request({
-      action: 'addChapter',
+      action: "addChapter",
       body: chapter,
     });
     const chapterMod = {
@@ -113,7 +139,7 @@ const SidebarContent = ({
     };
 
     const update = await Request({
-      action: 'updateChapterInModules',
+      action: "updateChapterInModules",
       body: { slug: moduleSlugName, chapter: chapterMod },
     });
     setChapters([
@@ -136,77 +162,40 @@ const SidebarContent = ({
     const url = `/${boardSlugName}/${levelSlugName}/${subjectSlugName}/${moduleSlugName}?chapterId=${chapter._id}&cardId=${cardData._id}`;
     FlowRouter.go(url);
   };
- 
-  const deleteChapter = async(chapter)=>{
-    
-    const deleteChapter =  await Request({
-      action: 'deleteChapter',
-      body: chapter._id ,
+
+  const deleteChapter = async (chapter) => {
+    const deleteChapter = await Request({
+      action: "deleteChapter",
+      body: chapter._id,
     });
-    const removeChapterRef =  await Request({
-      action: 'removeChapterRef',
-      body: chapter ,
+    const removeChapterRef = await Request({
+      action: "removeChapterRef",
+      body: chapter,
     });
-    const newState = allChapters.filter((item)=>item._id !==chapter._id)
+    const newState = allChapters.filter((item) => item._id !== chapter._id);
     setChapters(newState);
-  }
-  const onUpdateChapter = async()=>{
-
-  const updatechapterName =  await Request({
-    action: 'updateChapter',
-    body: selectedChapter ,
-  });
-  const update = await Request({
-    action: 'updateChapterInModules',
-    body: { slug: moduleSlugName, chapter: selectedChapter },
-  });
-  const newState = allChapters.map(obj =>
-    obj._id === selectedChapter._id ?selectedChapter : obj
-);
+  };
+  const onUpdateChapter = async () => {
+    const updatechapterName = await Request({
+      action: "updateChapter",
+      body: selectedChapter,
+    });
+    const update = await Request({
+      action: "updateChapterInModules",
+      body: { slug: moduleSlugName, chapter: selectedChapter },
+    });
+    const newState = allChapters.map((obj) =>
+      obj._id === selectedChapter._id ? selectedChapter : obj
+    );
     setChapters(newState);
-    onCloseEditPopup()
-  }
-
-  saveLastPosition = async()=>{
-
-    const progressValue =getProgressValue()
-    const urlToSave = `/${boardSlugName}/${levelSlugName}/${subjectSlugName}/${moduleSlugName}?chapterId=${chapterId}&cardId=${cardId}`
-    const subject = await Request({
-      action: "getSubjectBySlug",
-      body: subjectSlugName
-    });
-
-    const addLastPosition = await Request({
-      action: "addLastPosition",
-      body: {
-        userId:Meteor.userId(),
-        subject:{
-          id:Meteor.userId(),
-          position:urlToSave,
-          progress:progressValue,
-          moduleName:subject.moduleName,
-          boardName:subject.boardName,
-          levelName:subject.levelName,
-          subjectName:subject.name
-        }
-      }
-    });
-
-  }
+    onCloseEditPopup();
+  };
 
   return (
     <>
-      <FlexBox
-        align
-        justify
-        column
-        className="organism_sidebar-top"
-      >
+      <FlexBox align justify column className="organism_sidebar-top">
         <Link to="/">
-            <Image
-              className="organism_sidebar-logo"
-              src="/img/logo.png"
-            />
+          <Image className="organism_sidebar-logo" src="/img/logo.png" />
         </Link>
 
         <FlexBox
@@ -223,10 +212,7 @@ const SidebarContent = ({
               thin
             />
           </div>
-          <Text className="organism_sidebar-text">
-            {getProgressValue()}
-            %
-          </Text>
+          <Text className="organism_sidebar-text">{getProgressValue()}%</Text>
         </FlexBox>
         {withIcon && (
           <IconButton
@@ -244,51 +230,56 @@ const SidebarContent = ({
         </Title>
         {allChapters.map((chapter, i) => (
           <div key={i}>
-            <ListItem key={chapter._id} {...chapter} onCardClick={(cardData) => onCardClick(cardData, chapter)} />
+            <ListItem
+              key={chapter._id}
+              {...chapter}
+              onCardClick={(cardData) => onCardClick(cardData, chapter)}
+            />
             {isTeamRole && (
-
               <div>
-<button
-              className="Sbar-config MuiButtonBase-root MuiButton-root MuiButton-contained makeStyles-root-98 MuiButton-containedPrimary"
-              id={chapter._id}
-              onClick={() => { openEditPopup(chapter); }}
-            >
-              Edit
-            </button>
-            <button
-              className="Sbar-config MuiButtonBase-root MuiButton-root MuiButton-contained makeStyles-root-98 MuiButton-containedPrimary"
-              id={chapter._id}
-              onClick={() => { 
-                setShowConfirmDialog(true)
-                setSelectedChapter({...chapter })
-                }}
-            >
-              Delete
-            </button>
-</div>
+                <button
+                  className="Sbar-config MuiButtonBase-root MuiButton-root MuiButton-contained makeStyles-root-98 MuiButton-containedPrimary"
+                  id={chapter._id}
+                  onClick={() => {
+                    openEditPopup(chapter);
+                  }}
+                >
+                  Edit
+                </button>
+                <button
+                  className="Sbar-config MuiButtonBase-root MuiButton-root MuiButton-contained makeStyles-root-98 MuiButton-containedPrimary"
+                  id={chapter._id}
+                  onClick={() => {
+                    setShowConfirmDialog(true);
+                    setSelectedChapter({ ...chapter });
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
             )}
 
             {isTeamRole && (
-            <button
-              className="Ch-btns MuiButtonBase-root MuiButton-root MuiButton-contained makeStyles-root-98 MuiButton-containedPrimary"
-              id={chapter._id}
-              onClick={() => { addCard(chapter._id); }}
-            >
-              Add
-              Card
-            </button>
+              <button
+                className="Ch-btns MuiButtonBase-root MuiButton-root MuiButton-contained makeStyles-root-98 MuiButton-containedPrimary"
+                id={chapter._id}
+                onClick={() => {
+                  addCard(chapter._id);
+                }}
+              >
+                Add Card
+              </button>
             )}
-
           </div>
         ))}
 
-        {isTeamRole && (<Button className="Ch-btns" onClick={openPopup}>Add Chapter</Button>)}
-        
-        <ClosePopup
-          open={open}
-          onClose={onClose}
-          title="Add a new chapter"
-        >
+        {isTeamRole && (
+          <Button className="Ch-btns" onClick={openPopup}>
+            Add Chapter
+          </Button>
+        )}
+
+        <ClosePopup open={open} onClose={onClose} title="Add a new chapter">
           <TextField
             style={{ marginBottom: 20 }}
             onChange={onChange}
@@ -313,21 +304,20 @@ const SidebarContent = ({
           <Button onClick={onUpdateChapter}>Update</Button>
         </ClosePopup>
         <ConfirmationDialog
-        onClose={() => setShowConfirmDialog(false)}
-        isShow={showConfirmDialog}
-        onSubmit={()=>{deleteChapter(selectedChapter)}}
-      />
-
-
+          onClose={() => setShowConfirmDialog(false)}
+          isShow={showConfirmDialog}
+          onSubmit={() => {
+            deleteChapter(selectedChapter);
+          }}
+        />
       </div>
-
     </>
   );
 };
 
 SidebarContent.defaultProps = {
   withIcon: false,
-  handleDrawerClose: () => { },
+  handleDrawerClose: () => {},
   chapters: [],
 };
 
