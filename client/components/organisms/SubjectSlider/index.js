0fc95b4d8c712ad0bd0cdc6485810c34b04d43ae
abbas ashraf <amughal@digital-dividend.com>
2020-04-14 18:39:42 +0500

    arrows of slider

--- client/components/organisms/SubjectSlider/index.js
+++ client/components/organisms/SubjectSlider/index.js
@@ -1,18 +1,25 @@
-import React, {
-  useState, useRef, useCallback, useMemo,
-} from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import Slider from 'react-slick';
-import { GenerateBreakpoints } from '/client/utils';
-import { Arrow } from '/client/components/icons';
-import { usePermission } from '/client/contexts/permission';
-import { Title } from '/client/components/atoms';
-import { SubjectCard } from '/client/components/molecules';
-import './styles.scss';
+import React, { useState, useRef, useCallback, useMemo } from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import Slider from "react-slick";
+import { GenerateBreakpoints } from "/client/utils";
+import { Arrow } from "/client/components/icons";
+import { usePermission } from "/client/contexts/permission";
+import { Title, IconButton } from "/client/components/atoms";
+import { SubjectCard } from "/client/components/molecules";
+import "./styles.scss";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 
 const SubjectSlider = ({
-  subjects, isUserSubjects, user, className, boardSlugName, levelSlugName, boardId, levelId,
+  subjects,
+  isUserSubjects,
+  user,
+  className,
+  boardSlugName,
+  levelSlugName,
+  boardId,
+  levelId,
 }) => {
   const breakponts = useMemo(GenerateBreakpoints, []);
   const slider = useRef(null);
@@ -36,45 +43,47 @@ const SubjectSlider = ({
     }
   };
 
-
   return (
     <div
       className={clsx(
-        'organism_subject-slider',
-        subjects.length === 0 && 'no-data',
-        subjects.length <= slidesToShow && 'no-arrows',
-        className,
+        "organism_subject-slider",
+        subjects.length === 0 && "no-data",
+        subjects.length <= slidesToShow && "no-arrows",
+        className
       )}
     >
-      {subjects.length > 0
-        ? (
-          <>
-            {subjects.length > slidesToShow && (
-            <Arrow
-              onClick={onPrev}
+      {subjects.length > 0 ? (
+        <>
+          {subjects.length > slidesToShow && (
+            <IconButton
+              // disabled={buttonDisable}
               className="organism_subject-slider-arrow prev"
-            />
-            )}
-            <Slider
-              ref={ref}
-              arrows={false}
-              slidesToShow={4}
-              slidesToScroll={3}
-              className="center"
-              responsive={breakponts}
-              infinite={subjects.length > 3}
-              onReInit={onSlidesToShowCountChange}
+              onClick={onPrev}
             >
-              {subjects.map(({
-                _id: subjectId, 
-                name, 
-                boardName, 
-                levelName, 
+              <ArrowBackIosIcon />
+            </IconButton>
+          )}
+          <Slider
+            ref={ref}
+            arrows={false}
+            slidesToShow={4}
+            slidesToScroll={3}
+            className="center"
+            responsive={breakponts}
+            infinite={subjects.length > 3}
+            onReInit={onSlidesToShowCountChange}
+          >
+            {subjects.map(
+              ({
+                _id: subjectId,
+                name,
+                boardName,
+                levelName,
                 slug: subjectNameSlug,
                 board,
                 level,
-                boardSlugName:boardSlug,
-                levelSlugName:levelSlug
+                boardSlugName: boardSlug,
+                levelSlugName: levelSlug,
               }) => (
                 <SubjectCard
                   role={role}
@@ -85,31 +94,34 @@ const SubjectSlider = ({
                   code={`${boardName} ${levelName}`}
                   subjectName={name}
                   subjectNameSlug={subjectNameSlug}
-                  levelSlugName={levelSlugName|| levelSlug}
+                  levelSlugName={levelSlugName || levelSlug}
                   boardSlugName={boardSlugName || boardSlug}
                   boardId={boardId || board}
                   levelId={levelId || level}
                   user={user}
                 />
-              ))}
-            </Slider>
-            { subjects.length > slidesToShow && (
-            <Arrow
-              onClick={onNext}
-              className="organism_subject-slider-arrow next"
-            />
+              )
             )}
-          </>
-        )
-        : (
-          <Title variant="h6">Nothing to show</Title>
-        )}
+          </Slider>
+          {subjects.length > slidesToShow && (
+            <IconButton
+              // disabled={buttonDisable}
+              className="organism_subject-slider-arrow next"
+              onClick={onNext}
+            >
+              <ArrowForwardIosIcon />
+            </IconButton>
+          )}
+        </>
+      ) : (
+        <Title variant="h6">Nothing to show</Title>
+      )}
     </div>
   );
 };
 
 SubjectSlider.defaultProps = {
-  className: '',
+  className: "",
   isUserSubjects: false,
 };
 
