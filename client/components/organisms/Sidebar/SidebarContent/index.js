adbdeda2ebe804b6ac360dece13143c9c4371b98
Firdous Ali <fali@digital-dividend.com>
2020-04-14 15:22:15 +0500

    ref

--- client/components/organisms/Sidebar/SidebarContent/index.js
+++ client/components/organisms/Sidebar/SidebarContent/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Meteor } from "meteor/meteor";
 import PropTypes from "prop-types";
 import { lighten } from "@material-ui/core/styles";
 import {
